fb69657a626b2dad6d896b7d7735627ccdbd01a0
sievins <sievins@scottlogic.com>
2014-10-24 08:51:51 +0100

    Add week 4 - patmat - Huffman

---
+++ patmat/.worksheet/src/patmat.worksheet.scala
@@ -0,0 +1,8 @@
+package patmat
+
+import common._
+import Huffman._
+
+object worksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(81); val res$0 = 
+  Nil.length;System.out.println("""res0: Int = """ + $show(res$0))}
+}
---
+++ patmat/worksheet.sc
@@ -0,0 +1,8 @@
+package patmat
+
+import common._
+import Huffman._
+
+object worksheet {
+  Nil.length                                      //> res0: Int = 0
+}
